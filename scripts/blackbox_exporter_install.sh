687ce6cf3686a6951b2f88d5a09c53d37a46626c
Treboder <robert.kohn@gmx.net>
2023-01-18 15:15:31 +0100

    adjust blackbox_exporter_install.sh

--- scripts/blackbox_exporter_install.sh
+++ scripts/blackbox_exporter_install.sh
@@ -1,9 +1,9 @@
-   useradd --no-create-home --shell /bin/false blackbox_exporter
-   wget https://github.com/prometheus/blackbox_exporter/releases/download/v0.14.0/blackbox_exporter-0.14.0.linux-amd64.tar.gz
-   tar -xvf blackbox_exporter-0.14.0.linux-amd64.tar.gz
-   cp blackbox_exporter-0.14.0.linux-amd64/blackbox_exporter /usr/local/bin/blackbox_exporter
-   chown blackbox_exporter:blackbox_exporter /usr/local/bin/blackbox_exporter
-   rm -rf blackbox_exporter-0.14.0.linux-amd64*   
-   mkdir /etc/blackbox_exporter
-   vim /etc/blackbox_exporter/blackbox.yml
-   chown blackbox_exporter:blackbox_exporter /etc/blackbox_exporter/blackbox.yml
+sudo useradd --no-create-home --shell /bin/false blackbox_exporter
+wget https://github.com/prometheus/blackbox_exporter/releases/download/v0.14.0/blackbox_exporter-0.14.0.linux-amd64.tar.gz
+tar -xvf blackbox_exporter-0.14.0.linux-amd64.tar.gz
+sudo cp blackbox_exporter-0.14.0.linux-amd64/blackbox_exporter /usr/local/bin/blackbox_exporter
+sudo chown blackbox_exporter:blackbox_exporter /usr/local/bin/blackbox_exporter
+rm -rf blackbox_exporter-0.14.0.linux-amd64*   
+sudo mkdir /etc/blackbox_exporter
+sudo touch /etc/blackbox_exporter/blackbox.yml
+sudo chown blackbox_exporter:blackbox_exporter /etc/blackbox_exporter/blackbox.yml
