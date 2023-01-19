7d77683c3ba0d9ef255d6e0fc822ef020646fffb
KRO5RT <robert.kohn@bosch.com>
2023-01-19 09:35:30 +0100

    adjust node exporter quckstart

--- setup_web_app_server.sh
+++ setup_web_app_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Install node exporter"
 sh ./scripts/node_exporter_install.sh
-sudo cp configs/node_exporter.service /etc/systemd/system/node-exporter.service
+sudo cp configs/node_exporter.service /etc/systemd/system/node_exporter.service
 sh ./scripts/node_exporter_setup.sh
